63521474595d099965340c4463d7808f8758ac92
Thomas Browning <brownit89@gmail.com>
2019-05-06 23:48:10 -0700

    queries

---
+++ queries.sql
@@ -0,0 +1,6 @@
+--query to insert into
+
+insert into card (card_id, name, rarity, description, mana_cost, card_type)
+values (card_idInput, nameInput, rarityInput, descriptionInput, mana_costInput, card_typeInput);
+
+
