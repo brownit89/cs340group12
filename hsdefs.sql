6c2a73bc22b510ddf2b426159905cf445c01ebe6
Devin Sather <satherde@oregonstate.edu>
2019-05-06 22:01:28 -0700

    added card Abomination

--- hsdefs.sql
+++ hsdefs.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS card; 
 create table card (
 	card_id int not null,
 	card_name varchar(255) not null,
@@ -10,7 +11,7 @@ create table card (
 
 INSERT INTO card(card_id, card_name, mana_cost, rarity, description, card_type) VALUES (1, 'Abomination', 5, 'Rare', 'Deal 2 damage to ALL characters.', 'Neutral');
 
-
+DROP TABLE IF EXISTS card_weapon; 
 create table card_weapon (
 	card_id int not null,
 	weapon_id int not null,
@@ -21,6 +22,7 @@ create table card_weapon (
 	references weapon(weapon_id)
 );
 
+DROP TABLE IF EXISTS weapon; 
 create table weapon (
 	weapon_id int not null,
 	ap int not null,
@@ -28,6 +30,7 @@ create table weapon (
 	primary key (weapon_id)
 );
 
+DROP TABLE IF EXISTS weapon_mechanics; 
 create table weapon_mechanics (
 	weapon_id int not null,
 	mech_id int not null,
@@ -36,6 +39,7 @@ create table weapon_mechanics (
 	foreign key (weapon_id) references mechanics(mech_id)
 );
 
+DROP TABLE IF EXISTS card_minion; 
 create table card_minion (
 	card_id int not null,
 	minion_id int not null,
@@ -45,6 +49,7 @@ create table card_minion (
 );
 INSERT INTO card_minion (card_id, minion_id) VALUES (1, 1);
 
+DROP TABLE IF EXISTS minion; 
 create table minion (
 	minion_id int not null,
 	ap int not null,
@@ -54,6 +59,7 @@ create table minion (
 
 INSERT INTO minion (minion_id, ap, hp) VALUES (1, 4, 4);
 
+DROP TABLE IF EXISTS minion_mechanics; 
 create table minion_mechanics (
 	minion_id int not null,
 	mech_id int not null,
@@ -62,12 +68,19 @@ create table minion_mechanics (
 	foreign key (mech_id) references mechanics(mech_id)
 );
 
+INSERT INTO minion_mechanics (minion_id, mech_id) VALUES (1, 1);
+INSERT INTO minion_mechanics (minion_id, mech_id) VALUES (1, 2);
+
+DROP TABLE IF EXISTS mechanics; 
 create table mechanics (
 	mech_id int not null,
 	name varchar(255),
 	primary key (mech_id)
 );
+INSERT INTO mechanics (mech_id, name) VALUES (1, 'TAUNT');
+INSERT INTO mechanics (mech_id, name) VALUES (2, 'DEATHRATTLE');
 
+DROP TABLE IF EXISTS card_spell; 
 create table card_spell (
 	card_id int not null,
 	spell_id int not null,
@@ -76,6 +89,7 @@ create table card_spell (
 	foreign key (spell_id) references spell(spell_id)
 );
 
+DROP TABLE IF EXISTS spell; 
 create table spell (
 	spell_id int not null,
 	primary key (spell_id)
