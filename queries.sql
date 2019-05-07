e47a89d4fcdc17418f6d1e2d28cc5c5735849184
Thomas Browning <brownit89@gmail.com>
2019-05-07 12:18:42 -0700

    updated queries

--- queries.sql
+++ queries.sql
@@ -1,5 +1,13 @@
 --query to insert into
 
+select * from card;
+select * from minion;
+select * from spell;
+select * from weapon;
+update card
+	set rarity = "rare"
+	where name = "Shield Maiden";
+delete from card where name = "Dr. Boom";
 insert into card (card_id, name, rarity, description, mana_cost, card_type)
 values (card_idInput, nameInput, rarityInput, descriptionInput, mana_costInput, card_typeInput);
 
