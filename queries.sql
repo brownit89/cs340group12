6ce035b01db1fe668473ec4c0e3f1f28a0fb61fa
Thomas Browning <brownit89@gmail.com>
2019-05-13 22:47:52 -0700

    deleted auto-inc values from insert

--- queries.sql
+++ queries.sql
@@ -8,7 +8,7 @@ update card
 	set rarity = "rare"
 	where name = "Shield Maiden";
 delete from card where name = "Dr. Boom";
-insert into card (card_id, name, rarity, description, mana_cost, card_type)
-values (card_idInput, nameInput, rarityInput, descriptionInput, mana_costInput, card_typeInput);
+insert into card (name, rarity, description, mana_cost, card_type)
+values (nameInput, rarityInput, descriptionInput, mana_costInput, card_typeInput);
 
 
