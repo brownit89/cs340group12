e71f7fa3a211b862914bf9ebbc6959d85066ca00
Thomas Browning <brownit89@gmail.com>
2019-05-20 22:29:00 -0700

    added more queries

--- queries.sql
+++ queries.sql
@@ -8,3 +8,11 @@ update card
 delete from card where name = "Dr. Boom";
 insert into card (name, rarity, description, mana_cost, card_type)
 values (nameInput, rarityInput, descriptionInput, mana_costInput, card_typeInput);
+insert into minion (attack_power, health_power)
+values (attack_powerInput, health_powerInput);
+insert into weapon (attack_power, durability)
+values (attack_powerInput, durabilityInput);
+insert into spell (spell_type)
+values (spell_typeInput);
+insert into mechanics (name)
+values (nameInput);
