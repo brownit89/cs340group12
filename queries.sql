f0db04ec277295be91a450e5b0a8a24c05da3486
Devin Sather <satherde@oregonstate.edu>
2019-05-20 23:39:20 -0700

    added mechanics queries

--- queries.sql
+++ queries.sql
@@ -33,3 +33,19 @@ inner join weapon on card_weapon.weapon_id = weapon.weapon_id;
 select * from card
 inner join card_spell on card.card_id = card_spell.card_id
 inner join spell on card_spell.spell_id = spell.spell_id;
+
+
+select * from card
+inner join card_minion on card.card_id = card_minion.card_id
+inner join minion on card_minion.minion_id = minion.minion_id
+inner join minion_mechanics on minion.minion_id = minion_mechanics.minion_id
+inner join mechanics on minion_mechanics.mech_id = mechanics.mech_id;
+
+
+
+
+select * from card
+inner join card_weapon on card.card_id = card_weapon.card_id
+inner join weapon on card_weapon.weapon_id = weapon.weapon_id
+inner join weapon_mechanics on weapon.weapon_id = weapon_mechanics.weapon_id
+inner join mechanics on weapon_mechanics.mech_id = mechanics.mech_id;
