5e1b6ee452f30e6ca073472aad5d43f92b5a6ee6
Thomas Browning <brownit89@gmail.com>
2019-05-20 20:31:31 -0700

    updated minion.js

---
+++ minion.js
@@ -0,0 +1,54 @@
+module.exports = function(){
+	var express = require('express');
+	var router = express.Router();
+	
+	function serveMinions(req,res){
+		console.log("You asked me for some minions?")
+		var query = 'select minion_id, attack_power, health_power from minion';
+		var mysql = req.app.get('mysql');
+		var context = {};
+		
+		function handleRenderingOfMinions(error, results, fields){
+			console.log(error)
+			console.log(results)
+			console.log(fields)
+			//stores results of query inside context
+			context.minions = results;
+			res.render('minion', context)
+		}
+		//executes mysql query
+		mysql.pool.query(query, handleRenderingOfMinions)
+
+	}
+
+	function serveOneMinion(req,res){
+		console.log(req.params.minionID);
+		console.log(req.params);
+		minionID = req.params.minionID
+
+		var queryStr = "select minion_id, attack_power, health_power from minions where minion_id = ?"
+
+		var mysql = res.app.get('mysql')
+		var context = {};
+
+		function handleRenderingOfOneMinion(error, results, fields){
+			console.log("results are " + results)
+			context.minion = results[0]
+			console.log(context)
+
+		if(error){
+			console.log(error)
+			res.write(error)
+			res.end();
+		}else{
+			res.render('serverMinion', context);
+		}
+		}
+		var queryStr = mysql.poo.query(queryStr, minionID, handleRenderingOfOneMinion);
+	}
+
+	router.get('/', serveMinions);
+	router.get('/:minionID', serveOneMinion);
+	return router;
+
+}();
