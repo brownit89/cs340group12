7f04336b07ba3844e6bc25b14ee62e8e99137f92
Thomas Browning <brownit89@gmail.com>
2019-05-28 22:35:44 -0700

    updated manage, read cards

--- readCards.js
+++ readCards.js
@@ -63,19 +63,6 @@ module.exports = function(){
 	});
 }
 
-	function getMinion(res, mysql, context, id, complete){
-		var sql = mysql.pool.query("select * from minion where minion_id = ?");
-		var inserts = [id];
-		mysql.pool.query(sql, inserts, function(error, results, fields){
-	if(error){
-		res.write(JSON.stringify(error));
-		res.end();
-	}
-	context.minion = results[0];
-	complete();
-
-	});
-}
 
 	function getMinionMechanics(res, mysql, context, complete){
 		mysql.pool.query("select * from card inner join card_minion on card.card_id = card_minion.card_id inner join minion on card_minion.minion_id = minion.minion_id inner join minion_mechanics on minion.minion_id = minion_mechanics.minion_id inner join mechanics on minion_mechanics.mech_id = mechanics.mech_id;", function(error, results, fields){
@@ -104,7 +91,7 @@ module.exports = function(){
 router.get('/', function(req, res){
 	var callbackCount = 0;
 	var context = {};
-	context.jsscripts = ["deletecard.js"];
+	context.jsscripts = ["deleteCard.js"];
 	var mysql = req.app.get('mysql');
 	getCards(res, mysql, context, complete);
 	function complete(){
@@ -129,22 +116,6 @@ router.get('/:id', function(req, res){
 	}
 });
 
-router.post('/', function(req, res){
-	console.log(req.body)
-	var mysql = req.app.get('mysql');
-	var sql = "insert into card (card_name, rarity, description, mana_cost, card_type) values (?, ?, ?, ?, ?)";
-	var inserts = [req.body.card_name, req.body.rarity, req.body.description, req.body.mana_cost, req.body.card_type];
-	sql = mysql.pool.query(sql,inserts,function(error,results,fields){
-		if(error){
-			console.log(JSON.stringify(error))
-			res.write(JSON.stringify(error));
-			res.end();
-		}else{
-			res.redirect('/card');
-		}
-
-	});
-});
 
 
 router.delete('/:id', function(req, res){
@@ -162,7 +133,7 @@ router.delete('/:id', function(req, res){
 	}
 	})
 })
-
+/*
 router.post('/', function(req, res){
 	console.log(req.body)
 	var mysql = req.app.get('mysql');
@@ -179,7 +150,7 @@ router.post('/', function(req, res){
 
 	});
 });
-
+*/
 
 router.delete('/:id', function(req, res){
 	var mysql = req.app.get('mysql');
